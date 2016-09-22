c3c80995bed71a2c5c1ccc3e1d568991a6c567f9
sri9cob <sri9cob@BMHE1069887>
2016-09-22 08:55:07 +0530

    Puzzles: 1. Horse movement help updated. 2. Compression logic indentation

--- src/main/java/edu/srini/compress/CompressionLogic.java
+++ src/main/java/edu/srini/compress/CompressionLogic.java
@@ -3,9 +3,8 @@ package edu.srini.compress;
 import java.util.Scanner;
 
 /*
- * 
  * 1
- *  * 1 1
+ * 1 1
  * 2 1
  * 1 2 1 1
  * 1 1 1 2 2 1
@@ -13,18 +12,14 @@ import java.util.Scanner;
  * 1 3 1 1 2 2 2 1
  * 1 1 1 3 2 1 3 2 1 1
  * 3 1 1 3 1 2 1 1 1 3 1 2 2 1
- * 
  */
 public class CompressionLogic {
-	
-	
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		int seed = scanner.nextInt();
 		int line = scanner.nextInt();
 		scanner.close();
-
 		System.out.println(new CompressionLogic().getNthCompressedLine(seed, line));
 	}
 
@@ -34,7 +29,6 @@ public class CompressionLogic {
 
 	private boolean isValidInput(int seed, int line) {
 		System.out.printf("seed = %d, line=%d\n", seed, line);
-
 		if ((seed < 0 || seed >= 100) || (line < 0 || line > 25)) {
 			System.out.println("Invalid input... 0 < seed < 100 and 0 < line <= 25");
 			throw new IllegalArgumentException("Invalid input... Expecting: 0 < seed < 100 and 0 < line <= 25");
@@ -55,7 +49,6 @@ public class CompressionLogic {
 			while (scan.hasNext()) {
 				// read next int in current buffer
 				curNum = scan.nextInt();
-
 				// rise counter
 				if (count == 0 || curNum == prevNum) {
 					count++;
@@ -65,7 +58,6 @@ public class CompressionLogic {
 					appendCountNumPair(next, count, prevNum);
 					count = 1;
 				}
-
 				// end of line reached
 				if (!scan.hasNext()) {
 					appendCountNumPair(next, count, curNum);
@@ -76,16 +68,13 @@ public class CompressionLogic {
 					prevNum = curNum;
 				}
 			}
-
 			// copy "next" to "current" and make "next" empty
 			curr.delete(0, curr.length());
 			curr.append(next.toString());
 
 			next.delete(0, next.length());
 		}
-
 		return curr.toString();
-
 	}
 
 	private void appendCountNumPair(StringBuffer sb, int count, int number) {
